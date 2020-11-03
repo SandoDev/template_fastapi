9be13ac57b8052a535dd138226cb6f4b65201139
SandoDev <camilo.sandoval@zinobe.com>
2020-11-03 16:54:45 -0500

    added exceptions

---
+++ app/core/responses.py
@@ -0,0 +1,19 @@
+def format_response(response: dict):
+    """
+    General format for all api responses
+
+    Params
+    ------
+        response: dict
+            - success: bool
+                True if the result is as expected
+            - message: str
+                Service response message
+            - data: dict
+                Service response data
+    """
+    return {
+        "success": response.get('success'),
+        "message": response.get('message'),
+        "data": response.get('data_response'),
+    }
--- config/api.py
+++ config/api.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.encoders import jsonable_encoder
+from starlette.responses import JSONResponse
+from starlette.exceptions import HTTPException as StarletteHTTPException
 from littlenv import littlenv
+
 from config.settings import API_VERSION
 from config.urls import urls
 from app.core.admin_db.mongoengine.connection import connect_db, disconnect_db
+from app.core.responses import format_response
+
 
 littlenv.load()
 
@@ -38,3 +44,37 @@ app.add_event_handler(
     "shutdown",
     disconnect_db
 )
+
+
+@app.exception_handler(Exception)
+async def exception_handler(request, exc):
+    data = {}
+    code_status = exc.args[3]
+    data["success"] = exc.args[0]
+    data["message"] = exc.args[1]
+    data["data"] = exc.args[2]
+
+    return JSONResponse(
+        status_code=code_status,
+        content=jsonable_encoder(
+            format_response(data)
+        ),
+    )
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    print()
+    print('Si alguna vez aparece esto haganmelo saber, sandodev')
+    print()
+    data = {
+        "codRespuesta": exc.status_code,
+        "observaciones": str(exc.detail)
+    }
+
+    return JSONResponse(
+        status_code=exc.status_code,
+        content=jsonable_encoder(
+            format_response(data)
+        ),
+    )
