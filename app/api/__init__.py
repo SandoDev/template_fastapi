804f03456bf788a6e77a6d56281cb37293dd39ae
Camilo Sandoval <camilo.sandoval@zinobe.com>
2020-12-01 12:43:21 -0500

    change wsgi for take async operations

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,5 +1,5 @@
 """
-This package save the coupling with the framework, 
+This package save the coupling with the framework,
 the urls of each endpoint, the views and serializers.
 
 It is organized by packages by versions
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,5 @@
 # este modulo se puede usar para hacer debug
 import uvicorn
-from config.api import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8002)
+    uvicorn.run("config.api:app", host="0.0.0.0", port=8002)
